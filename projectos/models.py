d0e1de9af090f50db20413da52793ec0ad277993
bardack134 <bardack134@gmail.com>
2023-11-02 06:09:26 +0900

    construccionModelo

--- projectos/models.py
+++ projectos/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
 # Create your models here.
+class infoProyectos(models.Model):
+    titulo = models.CharField(max_length=100)
+
+    github_link = models.URLField(max_length=200)
+
+    introduccion = models.TextField()
+
+    herramientas = models.TextField()
+
+    resultadosLogros = models.TextField()
+
+    class Meta:
+        verbose_name_plural = 'infoProyectos'
+
+        verbose_name="infoProyecto"
+    
+    def __str__(self):
+        return self.titulo
